3dc6107c6435791f211f8ff37b557f0e19589f53
shyju <shyju.vishu@gmail.com>
2020-10-30 12:31:16 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import TinderCard from './components/TinderCard/TinderCard';
 
 function App() {
   return (
     <div className="app">
+      <Header></Header>
+      <TinderCard></TinderCard>
     </div>
   );
 }
